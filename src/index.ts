c379ac29087351b10370ac5ce3918e78adae56c8
Sam Cotterell <samuel.cotterell@gmail.com>
2023-11-06 21:17:00 +1000

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-/**
- * Required External Modules
- */
-
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -10,32 +6,18 @@ import { xScheduleRouter } from "./service";
 
 dotenv.config();
 
-/**
- * App Variables
- */
-
 if (!process.env.PORT) {
-    process.exit(1);
- }
- 
- const PORT: number = parseInt(process.env.PORT as string, 10);
- 
- const app = express();
+  process.exit(1);
+}
 
- /**
- *  App Configuration
- */
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/xschedule", xScheduleRouter);
 
-/**
- * Server Activation
- */
-
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+  console.log(`Listening on port ${PORT}`);
+});
